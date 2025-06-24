c2a6db2d1e95156d956feef8f078da134f269e58
Kade <140114827+KadeTheExploiter@users.noreply.github.com>
2025-06-24 22:48:45 +0200

    rewrite

--- main.cpp
+++ main.cpp
@@ -1,145 +1,111 @@
 #include <iostream>
-#include <cstdlib>
-#include <pthread.h>
-#include <string>
-#include <vector>
+#include <map>
 
 void clear() {
-#ifdef _WIN32 // FUCK YOU WINDOWS, YOU HEARD ME FUCK YOU.
-    std::system("cls");
-#else
-    std::system("clear");
-#endif
+    std::printf("\033[2J\033[1;1H");
 }
 
-int main() {
-  std::string url;
-  std::string command = "yt-dlp --embed-metadata ";
-  std::string prompt;
-  std::string option;
-
-  clear();
-
-  std::cout << "lookup method? \n - [1] = url, \n - [2] = ytsearch \n - [x] = exit program \nInput: ";
-  std::cin >> option;
-
-  int methodchosen = 0;
-  if (option == "x") {
-    return 0;
-  }
-  try {
-    methodchosen = std::stoi(option);
-  } catch (...) {
-    std::cout << "\n sybau </3";
-    return 0;
-  }
-
-  clear();
-
-  if (methodchosen == 1) {
-    std::cout << "Input URL:";
-    std::cin >> url;
-  } else if (methodchosen == 2) {
-    std::string search = "";
-    std::cout << "Search for:";
-    std::getline(std::cin >> std::ws, search);
-
-    if (search == "") {
-      std::cout << "Search is empty, cfdasfsfdsfdwwhbwreancelling";
-      return 0;
+int to_number(std::string& line, int fail_safe = 1) {
+    try {
+        return std::stoi(line);
+    } catch (const std::invalid_argument& e) {
+        std::printf("unknown character, selecting 1");
     }
 
-    std::vector<std::pair<std::string, std::string>> searchresults;
-    std::string lookupcommand = "yt-dlp --flat-playlist --print \"%(title)s || %(id)s\" \"ytsearch5:" + search + "\"";
+    return fail_safe;
+}
+
+auto get_input(std::string custom_prompt) {
     clear();
 
-    FILE* pipe = popen(lookupcommand.c_str(), "r");
+    std::string search_input;
+    std::cout << custom_prompt;
+    std::getline(std::cin >> std::ws, search_input);
+
+    clear();
+    return search_input;
+}
+
+auto search_query(std::string search_input) {
+    std::map<int, std::string> query_results;
+    std::string search_up_command = "yt-dlp --flat-playlist --print \"%(title)s || %(id)s\" \"ytsearch5:" + search_input + "\"";
+    std::string song_selected;
+
+    auto pipe = popen(search_up_command.c_str(), "r");
+
     char buffer[512];
     int count = 0;
     while (fgets(buffer, sizeof(buffer), pipe) !=nullptr) {
-      std::string line(buffer);
+        std::string line(buffer);
 
-      if (!line.empty() && line.back() == '\n') {
-        line.pop_back();
-      }
+        if (!line.empty() && line.back() == '\n') {
+            line.pop_back();
+        }
 
-      size_t seperated = line.find(" || ");
-      if (seperated != std::string::npos) {
-        count ++;
+        size_t seperated = line.find(" || ");
+        if (seperated != std::string::npos) {
+            count ++;
 
-        std::string title = line.substr(0, seperated);
-        std::string id = line.substr(seperated + 4);
+            std::string title = line.substr(0, seperated);
+            std::string id = line.substr(seperated + 4);
 
-        std::cout << "[" + std::to_string(count) + "] " + line << "\n";
-        searchresults.emplace_back(title, id);
-      }
+            std::cout << "[" + std::to_string(count) + "] " + line << "\n";
+            query_results[count] = id;
+        }
     }
 
     pclose(pipe);
+    return query_results;
+}
 
-    // std::system(lookupcommand.c_str());
-    std::string option = "";
+int main() {
+    std::string file_format;
+    std::string full_url;
+    std::string command = "yt-dlp --embed-metadata --sleep-requests .5 --sleep-interval .5 --retry-sleep fragment:300 ";
+    std::string look_up_option = get_input("Look Up Method? \n - [1] = url, \n - [2] = ytsearch \n - [x] = exit program \nInput: ");
 
-    std::cout << "select a number to continue, otherwise just press x and go\n";
-    std::cout << ":3 input:";
-
-    std::cin >> option;
-    if (option == "x") {
-      return 0;
+    if (look_up_option == "x") {
+        return 0;
     }
 
-    int selected = 0;
-    try {
-        selected = std::stoi(option);
-    } catch (...) {
-        std::cerr << "wow youre so quirky n fun!!!!!!! lolol\n";
+    int search_option = to_number(look_up_option);
+
+    if (search_option == 1) {
+        full_url = get_input("Input URL: ");
+    } else {
+        search_again:
+
+        std::string search_input = get_input("Search: ");
+        std::map<int, std::string> query_results = search_query(search_input);
+
+        std::cout << "Select number [or z to retry search]: ";
+        std::getline(std::cin >> std::ws, search_input);
+
+        if (search_input == "z") {
+            goto search_again;
+        }
+
+        int selected_choice = to_number(search_input);
+        full_url = "https://youtube.com/watch?v=" + query_results[selected_choice];
+    }
+
+    file_format = get_input("file format? \n - [1] = mp3 \n - [2] = mp4 \nInput: ");
+
+    int file_format_int = to_number(file_format);
+
+    if (file_format_int == 2) {
+        command += "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' ";
+    } else {
+        command += "-x --audio-format mp3 ";
+    }
+
+    std::string execute_command = command + "\"" + full_url + "\"";
+    int out = std::system(execute_command.c_str());
+
+    if (out != 0) {
+        std::cerr << "Song failed to download, Error code: " + std::to_string(out);
         return 1;
     }
 
-    clear();
-
-    if (selected >= 1 and selected <= searchresults.size()) {
-      url = "https://youtube.com/watch?v=" + searchresults[selected - 1].second;
-    } else {
-      std::cout << "think you are funny huh?";
-      return 0;
-    }
-  }
-
-  std::cout << "file format? \n - [1] = mp3 \n - [2] = mp4 \n - [x] = exit program \nInput: ";
-  std::cin >> prompt;
-  int selectedformat = 0;
-  if (prompt == "x") {
     return 0;
-  }
-
-  try {
-    selectedformat = std::stoi(prompt);
-  } catch (...) {
-      std::cerr << "try again bub, or else your bembership is in danger...\n";
-      return 1;
-  }
-  if (selectedformat == 1) {
-    command += "-x --audio-format mp3 ";
-  } else if (selectedformat == 2) {
-    command += "-f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' ";
-  } else {
-    std::cout << "\n whatever, fuck off and heres your mp3 \n";
-    command += "-x --audio-format mp3 ";
-  }
-
-  clear();
-
-  std::string info = "Selected:" + prompt + "\n";
-  //std::cout << info;
-
-  std::string execute_command = command + "\"" + url + "\"";
-  int out = std::system(execute_command.c_str());
-
-  if (out!=0) {
-    std::cerr << "song failed to download idk why but hees a code numbar.." + std::to_string(out);
-    return 1;
-  }
-
-  return 0;
 }
